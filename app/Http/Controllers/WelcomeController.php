3cd22860fd15b2a5e2ab65b8f1b97f56dff1e9cb
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-09-03 18:06:30 +0900

    add:new files

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+  public function contact()  // 追加
+    {
+        return view('contact'); // (a) view 関数を使わず、テキストを返してみる
+    }
+
+  public function index()
+  {
+      return view('welcome');
+  }
 }

1b3273fa7af6b78a31be80bd8ac444bf51fca94d
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-09-04 10:09:55 +0900

    add: seeder file

--- app/Article.php
+++ app/Article.php
@@ -7,19 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'body', 'published_at'];
+    protected $dates = ['published_at'];
+    
 
     public function getTitleAttribute($value)
     {
         // 大文字に変換
         return mb_strtoupper($value);
-    };
+    }
 
     public function setTitleAttribute($value)
     {
         // 小文字に変換
         $this->attributes['title'] = mb_strtolower($value);
-    };
+    }
 
-    protected $dates = ['published_at'];
+
 
 }
